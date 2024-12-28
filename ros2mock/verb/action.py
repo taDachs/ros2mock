1b40c2187afeca215218f16f80ddfcc7249b5c27
Max Schik <max.schik@googlemail.com>
2024-12-28 20:01:59 +0000

    adds timeout option for mock services and actions

--- ros2mock/verb/action.py
+++ ros2mock/verb/action.py
@@ -1,6 +1,7 @@
 import importlib
 
 import rclpy
+from rclpy.executors import MultiThreadedExecutor
 from ros2cli.node import NODE_NAME_PREFIX
 from ros2action.api import action_name_completer, action_type_completer
 from ros2mock.api import ActionResultCompleter, ActionFeedbackCompleter, edit_string_in_editor
@@ -9,90 +10,137 @@ from rosidl_runtime_py import set_message_fields, message_to_yaml
 from rclpy.action import ActionServer
 import yaml
 
+
 class ActionVerb(VerbExtension):
     """Mock an action."""
 
     def add_arguments(self, parser, cli_name):
         arg = parser.add_argument(
-            'action_name',
-            help="Name of the ROS action to mock to (e.g. '/add_two_ints')")
+            "action_name", help="Name of the ROS action to mock to (e.g. '/add_two_ints')"
+        )
         arg.completer = action_name_completer
         arg = parser.add_argument(
-            'action_type',
-            help="Type of the ROS action (e.g. 'example_interfaces/action/Fibonacci')")
+            "action_type",
+            help="Type of the ROS action (e.g. 'example_interfaces/action/Fibonacci')",
+        )
         arg.completer = action_type_completer
         arg = parser.add_argument(
-            'values', nargs='?', default='{}',
-            help='Values to fill the action result with in YAML format ' +
-                 "(e.g. '{a: 1, b: 2}'), " +
-                 'otherwise the action result will be published with default values')
-        arg.completer = ActionResultCompleter(
-            action_type_key='action_type')
+            "values",
+            nargs="?",
+            default="{}",
+            help="Values to fill the action result with in YAML format "
+            + "(e.g. '{a: 1, b: 2}'), "
+            + "otherwise the action result will be published with default values",
+        )
+        arg.completer = ActionResultCompleter(action_type_key="action_type")
         arg = parser.add_argument(
-            '--feedback', nargs='?',
+            "--feedback",
+            nargs="?",
             action="append",
-            help='Values to be used as feedback, can be used multiple times. Get returned in sequence')
-        arg.completer = ActionFeedbackCompleter(
-            action_type_key='action_type')
+            help="Values to be used as feedback, can be used multiple times. Get returned in sequence",
+        )
+        arg.completer = ActionFeedbackCompleter(action_type_key="action_type")
 
-        arg = parser.add_argument('--abort', action='store_true', help='Abort the goal.')
+        arg = parser.add_argument("--abort", action="store_true", help="Abort the goal.")
 
-        arg = parser.add_argument('--editor',
-            action="store_true", help="If set, opens the editor on result")
+        arg = parser.add_argument(
+            "--editor", action="store_true", help="If set, opens the editor on result"
+        )
+
+        arg = parser.add_argument(
+            "--timeout",
+            type=float,
+            default=None,
+            help="Timeout before result or next feedback is sent",
+        )
 
     def main(self, *, args):
         return mocker(
-            args.action_type, args.action_name, args.values, args.feedback, args.editor, args.abort)
+            args.action_type,
+            args.action_name,
+            args.values,
+            args.feedback,
+            args.editor,
+            args.abort,
+            args.timeout,
+        )
 
 
-def mocker(action_type, action_name, values, feedbacks, editor, abort):
+def mocker(
+    action_type: str,
+    action_name: str,
+    values: str,
+    feedbacks: list[str],
+    editor: bool,
+    abort: bool,
+    timeout: float | None,
+):
     try:
-        parts = action_type.split('/')
+        parts = action_type.split("/")
         if len(parts) == 2:
-            parts = [parts[0], 'srv', parts[1]]
+            parts = [parts[0], "srv", parts[1]]
         package_name = parts[0]
-        module = importlib.import_module('.'.join(parts[:-1]))
+        module = importlib.import_module(".".join(parts[:-1]))
         action_type_name = parts[-1]
         action_module = getattr(module, action_type_name)
     except (AttributeError, ModuleNotFoundError, ValueError):
-        raise RuntimeError('The passed action type is invalid')
+        raise RuntimeError("The passed action type is invalid")
     try:
         action_module.Goal
         action_module.Result
         action_module.Feedback
     except AttributeError:
-        raise RuntimeError('The passed type is not a action')
+        raise RuntimeError("The passed type is not a action")
 
     values_dictionary = yaml.safe_load(values)
 
     rclpy.init()
-    node = rclpy.create_node(NODE_NAME_PREFIX + '_action_mocker_%s_%s' % (package_name, action_type_name))
+    node = rclpy.create_node(
+        NODE_NAME_PREFIX + "_action_mocker_%s_%s" % (package_name, action_type_name)
+    )
+
+    if timeout:
+        wait_rate = node.create_rate(1.0 / timeout)
 
     result = action_module.Result()
     try:
         set_message_fields(result, values_dictionary)
     except Exception as e:
-        return 'Failed to populate field: {0}'.format(e)
+        return "Failed to populate field: {0}".format(e)
 
     def generic_callback(goal_handle):
+        node.get_logger().info("Received goal request")
         if editor:
             response_string = edit_string_in_editor(message_to_yaml(result))
-            set_message_fields(result, yaml.safe_load(response_string))
+            try:
+                set_message_fields(result, yaml.safe_load(response_string))
+            except Exception as e:
+                return "Failed to populate field: {0}".format(e)
+
         feedback = action_module.Feedback()
+
         for f in feedbacks:
-            f = f or '{}' # in case it's None
+            if timeout:
+                wait_rate.sleep()
+            f = f or "{}"  # in case it's None
             set_message_fields(feedback, yaml.safe_load(f))
             goal_handle.publish_feedback(feedback)
+            node.get_logger().info("Published Feedback")
+
         if abort:
             goal_handle.abort()
             return result
+
+        if timeout:
+            wait_rate.sleep()
         goal_handle.succeed()
+        node.get_logger().info("Returned Result")
         return result
 
     server = ActionServer(node, action_module, action_name, generic_callback)
 
-    rclpy.spin(node)
+    exec = MultiThreadedExecutor()
+    rclpy.spin(node, executor=exec)
 
     node.destroy_node()
     rclpy.shutdown()
-
--- ros2mock/verb/service.py
+++ ros2mock/verb/service.py
@@ -1,6 +1,7 @@
 import importlib
 
 import rclpy
+from rclpy.executors import MultiThreadedExecutor
 from ros2cli.node import NODE_NAME_PREFIX
 from ros2service.api import ServiceNameCompleter, ServiceTypeCompleter
 from ros2mock.api import ServiceResponseCompleter, edit_string_in_editor
@@ -8,74 +9,87 @@ from ros2mock.verb import VerbExtension
 from rosidl_runtime_py import set_message_fields, message_to_yaml
 import yaml
 
+
 class ServiceVerb(VerbExtension):
     """Mock a service."""
 
     def add_arguments(self, parser, cli_name):
         arg = parser.add_argument(
-            'service_name',
-            help="Name of the ROS service to mock to (e.g. '/add_two_ints')")
-        arg.completer = ServiceNameCompleter(
-            include_hidden_services_key='include_hidden_services')
+            "service_name", help="Name of the ROS service to mock to (e.g. '/add_two_ints')"
+        )
+        arg.completer = ServiceNameCompleter(include_hidden_services_key="include_hidden_services")
         arg = parser.add_argument(
-            'service_type',
-            help="Type of the ROS service (e.g. 'std_srvs/srv/Empty')")
-        arg.completer = ServiceTypeCompleter(
-            service_name_key='service_name')
+            "service_type", help="Type of the ROS service (e.g. 'std_srvs/srv/Empty')"
+        )
+        arg.completer = ServiceTypeCompleter(service_name_key="service_name")
         arg = parser.add_argument(
-            'values', nargs='?', default='{}',
-            help='Values to fill the service response with in YAML format ' +
-                 "(e.g. '{a: 1, b: 2}'), " +
-                 'otherwise the service request will be published with default values')
-        arg.completer = ServiceResponseCompleter(
-            service_type_key='service_type')
-        arg = parser.add_argument('--editor',
-            action="store_true", help="If set, opens the editor on response")
+            "values",
+            nargs="?",
+            default="{}",
+            help="Values to fill the service response with in YAML format "
+            + "(e.g. '{a: 1, b: 2}'), "
+            + "otherwise the service request will be published with default values",
+        )
+        arg.completer = ServiceResponseCompleter(service_type_key="service_type")
+        arg = parser.add_argument(
+            "--editor", action="store_true", help="If set, opens the editor on response"
+        )
+        arg = parser.add_argument(
+            "--timeout",
+            type=float,
+            default=None,
+            help="Timeout before result or next feedback is sent",
+        )
 
     def main(self, *, args):
-        return mocker(
-            args.service_type, args.service_name, args.values, args.editor)
+        return mocker(args.service_type, args.service_name, args.values, args.editor, args.timeout)
 
 
-def mocker(service_type, service_name, values, editor):
+def mocker(service_type: str, service_name: str, values: str, editor: bool, timeout: float | None):
     try:
-        parts = service_type.split('/')
+        parts = service_type.split("/")
         if len(parts) == 2:
-            parts = [parts[0], 'srv', parts[1]]
+            parts = [parts[0], "srv", parts[1]]
         package_name = parts[0]
-        module = importlib.import_module('.'.join(parts[:-1]))
+        module = importlib.import_module(".".join(parts[:-1]))
         srv_name = parts[-1]
         srv_module = getattr(module, srv_name)
     except (AttributeError, ModuleNotFoundError, ValueError):
-        raise RuntimeError('The passed service type is invalid')
+        raise RuntimeError("The passed service type is invalid")
     try:
         srv_module.Request
         srv_module.Response
     except AttributeError:
-        raise RuntimeError('The passed type is not a service')
+        raise RuntimeError("The passed type is not a service")
 
     values_dictionary = yaml.safe_load(values)
 
     rclpy.init()
-    node = rclpy.create_node(NODE_NAME_PREFIX + '_requester_%s_%s' % (package_name, srv_name))
+    node = rclpy.create_node(NODE_NAME_PREFIX + "_requester_%s_%s" % (package_name, srv_name))
+
+    if timeout:
+        wait_rate = node.create_rate(1.0 / timeout)
 
     response = srv_module.Response()
     try:
         set_message_fields(response, values_dictionary)
     except Exception as e:
-        return 'Failed to populate field: {0}'.format(e)
+        return "Failed to populate field: {0}".format(e)
 
-    def generic_callback(req, _):
-        node.get_logger().debug(f"Request: {req}")
+    def generic_callback(*_):
+        node.get_logger().info("Received Request")
         if editor:
             response_string = edit_string_in_editor(message_to_yaml(response))
             set_message_fields(response, yaml.safe_load(response_string))
+        if timeout:
+            wait_rate.sleep()
+        node.get_logger().info("Returning Response")
         return response
 
     node.create_service(srv_module, service_name, generic_callback)
 
-    rclpy.spin(node)
+    exec = MultiThreadedExecutor()
+    rclpy.spin(node, executor=exec)
 
     node.destroy_node()
     rclpy.shutdown()
-
